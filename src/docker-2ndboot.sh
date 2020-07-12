fd8a9d40270a9b48ac4829971d7a5c60d97910e3
lekai63 <lekai63@gmail.com>
2020-07-12 22:18:25 +0800

    fix x11vnc to repeat forever

--- src/docker-2ndboot.sh
+++ src/docker-2ndboot.sh
@@ -24,7 +24,8 @@ export DISPLAY=:0
 until [ -e /tmp/.X11-unix/X0 ]; do sleep 1; done
 : FIXME: remove this sleep
 sleep 1
-x11vnc -usepw &
+# x11vnc -usepw &
+x11vnc -repeat -forever -display :1 -xkb -usepw &
 : FIXME: remove this sleep
 sleep 1
 fvwm &
